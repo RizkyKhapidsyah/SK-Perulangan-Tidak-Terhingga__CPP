6a79ea274b6fe397529b425e13cbe419cf3d702a
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-01-06 20:37:02 +0700

    Add project files.

---
+++ SK-Perulangan-Tidak-Terhingga__CPP/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+/* Created by Rizky Khapidsyah */
+
+int main() {
+	int bil;
+	
+	for (bil = 60; bil >= 10; bil++) {
+		//printf("%d", bil);
+		cout << bil;
+	}
+		
+	_getch();
+	return 0;
+}
